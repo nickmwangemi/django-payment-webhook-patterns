e9906ae2542146e5c5bc396d5b348f5f91cd1f40
nickmwangemi <nickmwangemi@gmail.com>
2026-06-04 08:15:40 +0300

    add database path env

--- config/settings.py
+++ config/settings.py
@@ -24,7 +24,7 @@ WSGI_APPLICATION = "config.wsgi.application"
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "NAME": os.environ.get("DATABASE_PATH", str(BASE_DIR / "db.sqlite3")),
     }
 }
 
